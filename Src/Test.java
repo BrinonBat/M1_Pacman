5985030cde1e43617e6072096a45f3daebc1c741
etudiant <etudiant@d2L2>
2020-09-29 15:23:53 +0200

    ajout de la classe Test

---
+++ Src/Test.java
@@ -0,0 +1,9 @@
+public class Test {
+   
+    public static void main(String[] args) {
+        SimpleGame game = new SimpleGame();
+        game.init();
+        //game.step();
+        //game.run();
+    }
+}
