f373b13e8873ee40990d27ca3cb3b2b85cb8d97b
alex <alex@dBRT>
2020-09-29 16:46:43 +0200

    Modification des fichiers Game et Test

--- Src/Test.java
+++ Src/Test.java
@@ -3,6 +3,7 @@ public class Test {
     public static void main(String[] args) {
         SimpleGame game = new SimpleGame();
         game.init();
+        game.takeTurn();
         //game.step();
         //game.run();
     }
