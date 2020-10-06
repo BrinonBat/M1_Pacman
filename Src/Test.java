5827fdfbe9bbb5620ebbf7c512d724b641e4b285
etudiant <etudiant@d2L2>
2020-10-06 17:10:38 +0200

    mise en place de vrai test

--- Src/Test.java
+++ Src/Test.java
@@ -2,12 +2,12 @@
 public class Test {
 
     public static void main(String[] args) {
-     
-      ViewSimpleGame game = new ViewSimpleGame();
-      ViewCommand command = new ViewCommand();
+      
       try{
-        Maze labyrinthe = new Maze("../Layouts/bigCorners.lay");
+        Maze labyrinthe = new Maze("Layouts/bigCorners.lay");
         PanelPacmanGame pacmanGame = new PanelPacmanGame(labyrinthe);
+        SimpleGame pacman = new SimpleGame();
+        ControllerSimpleGame control = new ControllerSimpleGame(pacman);
       }
       catch(Exception e){
           System.out.println("Il y'a une erreur du type : "+ e.getMessage());
