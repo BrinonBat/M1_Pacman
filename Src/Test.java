60acb03634919b7636dc7ceb4bc1adcab3dd5204
alex <alex@dBRT>
2020-10-06 15:56:28 +0200

    Interface ViewCommande

--- Src/Test.java
+++ Src/Test.java
@@ -4,8 +4,9 @@ public class Test {
     public static void main(String[] args) {
      
 
-    ViewSimpleGame game = new ViewSimpleGame();
+    //ViewSimpleGame game = new ViewSimpleGame();
     ViewCommand command = new ViewCommand();
+   
     try{
       Maze labyrinthe = new Maze("../Layouts/bigCorners.lay");
       PanelPacmanGame pacmanGame = new PanelPacmanGame(labyrinthe);
@@ -14,5 +15,5 @@ public class Test {
      {
          System.out.println("Il y'a une erreur du type : "+ e.getMessage());
      }
-    }
+}
 }
--- Src/ViewCommand.java
+++ Src/ViewCommand.java
@@ -1,7 +1,16 @@
+import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.Toolkit;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JSlider;
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
+import javax.swing.plaf.basic.BasicOptionPaneUI.ButtonActionListener;
+
+import java.awt.GridLayout;
 
 public class ViewCommand extends JFrame {
 
@@ -17,7 +26,27 @@ public class ViewCommand extends JFrame {
 
         jFrame.setSize(width/2 , height/2);
         jFrame.setLocationRelativeTo(null);
-      
+
+        GridLayout conteneur = new GridLayout(2,2);
+        jFrame.setLayout(conteneur);
+
+
+        JPanel buttonHaut = new JPanel(new GridLayout(1,4));
+        JPanel buttonBas = new JPanel(new GridLayout(1,2));
+
+
+        buttonHaut.add(new JButton("", new ImageIcon("icones/icon_restart.png")));
+        buttonHaut.add(new JButton("", new ImageIcon("icones/icon_run.png")));
+        buttonHaut.add(new JButton("", new ImageIcon("icones/icon_step.png")));
+        buttonHaut.add(new JButton("", new ImageIcon("icones/icon_pause.png")));
+
+        buttonBas.add(new JSlider(0,10,10));
+        buttonBas.add(new JButton("bonjour"));
+
+        jFrame.add(buttonHaut);
+
+        jFrame.add(buttonBas);
+
 
         jFrame.setVisible(true);
     }
