fa0e6df3240ea5e8d9677e5b318d32260e7d49ec
BrinonBat <baptiste@brinon.eu>
2020-11-17 17:31:43 +0100

    stratégie ghost avance

--- Src/StrategieGhost.java
+++ Src/StrategieGhost.java
@@ -18,9 +18,9 @@ public class StrategieGhost implements Strategie {
       return move;
     }
 
-
-    protected int shortestway(Agent p,Agent pacmanPos,Maze labyrinthe)
-    {
+    // sortie : int compris entre 1 et 4 en fonction du numero du coup à faire.
+    protected int shortestway(Agent ghost,Agent pacmanPos,Maze labyrinthe)
+    { /*
       int x = labyrinthe.getSizeX();
       int y = labyrinthe.getSizeY();
       double[][] possibleMove = new double[x][y];
@@ -33,13 +33,40 @@ public class StrategieGhost implements Strategie {
              possibleMove[x][y] = -99999;
            }
          }
-       }
+       }*/
 
-      
-      
+       /* rappel :
+          NORTH = 0;
+          SOUTH = 1;
+          EAST = 2;
+          WEST = 3; */
+          int nMov;
+       int diffX=ghost.getPosition().getX()-pacmanPos.getPosition().getX();
+       int diffY=ghost.getPosition().getY()-pacmanPos.getPosition().getY();
+       int direction;
+       if (Math.abs(diffX)>Math.abs(diffY) and){
+          if(diffX>0){ nMov=cherche_chemin(3,ghost.getPosition(),pacmanPos.getPosition(),labyrinthe,0); }
+          else {nMov=cherche_chemin(2,ghost.getPosition(),pacmanPos.getPosition(),labyrinthe,0);}
+       } else {
+        if(diffX>0){ cherche_chemin(3,ghost.getPosition(),pacmanPos.getPosition(),labyrinthe,0); }
+        else {cherche_chemin(2,ghost.getPosition(),pacmanPos.getPosition(),labyrinthe,0);}
+       }
+       //parcours les 4 coups possibles
+              for(int actuel=1; actuel<4; i++){
+         if(cherche_chemin(actuel,ghost.getPosition(),pacmanPos.getPosition(),labyrinthe,0)<nMov)
+       }
        return 1;
         
     }
-
-  
+    private int cherche_chemin(int dir,PositionAgent ghostPos, PositionAgent pacmanPos,Maze labyrinthe, int taille){
+      switch(dir){
+        case(0):{
+          
+        }
+      }
+      for(int actuel=1; actuel<4; i++){
+        if(cherche_chemin(actuel,ghostPos,pacmanPos,labyrinthe,0))
+      }
+    }
+    
 }
