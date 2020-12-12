22f93320ddadbf74a9ec76a1caf3dda0b10fb429
BrinonBat <baptiste@brinon.eu>
2020-12-12 20:35:16 +0100

    correction Main

--- Src/Test.java
+++ Src/Test.java
@@ -3,7 +3,7 @@ public class Test {
 
     public static void main(String[] args) {
       PacmanGame game = new PacmanGame();
-      ViewCommand view = new ViewCommand(new ControllerPacmanGame(game));
+      new ViewCommand(new ControllerPacmanGame(game));
       
     }
 }
